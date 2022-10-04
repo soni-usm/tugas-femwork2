bc117cbc952efccd5c4f52c2860b2eb2ddcc38f0
soni-usm <102939393+soni-usm@users.noreply.github.com>
2022-10-04 22:58:52 +0700

    tugas menejemen femwork

---
+++ tugas femwork2.php
@@ -0,0 +1,21 @@
+<?
+class libery {
+
+public $buku;
+public $judul; 
+public $pengarang; 
+public $seri; 
+public $rakbuku;
+
+public function construct($title,$author,$series,$bookshelf)
+{
+     $this->buku = $book;
+	 $this->judul = $title;
+	 $this->pengarang = $author;
+     $this->seri = $series;
+     $this->rakbuku = $bookshelf;
+}
+
+}
+
+?>
